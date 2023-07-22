ff980e0d2901ee54fe6dcf8e0235ca5e77744a10
Sergio Brü <89431263+zzergAtStage@users.noreply.github.com>
2023-07-23 01:58:50 +0200

    creating a structures and model

---
+++ src/Model/Store.java
@@ -0,0 +1,12 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Store {
+    private ArrayList<Toy> stores;
+
+    private PriorityQueue<Toy> promo_storage;
+
+
+}
