bddc3985d7e9b2c8152b70a428dc2274b8945b7b
Sergio Brü <89431263+zzergAtStage@users.noreply.github.com>
2023-07-23 00:49:48 +0200

    initial commit

---
+++ src/Model/Toy.java
@@ -0,0 +1,36 @@
+package Model;
+
+public class Toy {
+    private final int id;
+
+    private final String name;
+
+    private final double selection_weight;
+
+    public Toy(int id, String name, double selection_weight) {
+        this.id = id;
+        this.name = name;
+        this.selection_weight = selection_weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSelection_weight() {
+        return selection_weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Toy {" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", selection_weight=" + selection_weight +
+                '}';
+    }
+}
